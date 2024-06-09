b00490dcca148f90d71f9883f7ff923a49d2f691
kamlangek1devops <DEV.OPS.211123@gmail.com>
2024-06-09 15:41:51 +0000

    push whole project service 2

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', function(req, res){
+        res.send("This is a app2 version :1.0.1");
+});
+
+app.listen(8080,function(){
+        console.log('app is running in port 8080');
+})
+
